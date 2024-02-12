a319ffb67befccc8cc2076438bcf0d1c2210514b
kaisamira <thienthan726@gmail.com>
2024-02-13 01:46:56 +0700

    #2 feat: update animation for navbar

--- src/assets/icons/FluentMdl2BulletedListText.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export function FluentMdl2BulletedListText(props) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 2048 2048" {...props}><path fill="currentColor" d="M384 1280v-128h1664v128zm0 384v-128h1664v128zm0-768V768h1664v128zm0-512h1664v128H384z"></path></svg>
-  )
-}
-export default FluentMdl2BulletedListText
--- src/components/_Header.jsx
+++ src/components/_Header.jsx
@@ -1,41 +1,20 @@
 import { useState } from 'react';
-import MenuLine from '../assets/icons/FluentMdl2BulletedListText'
+import MenuHamburger from '../assets/icons/FluentMdl2GlobalNavButton'
+import ButtonMenu from './_ButtonMenu';
 function Header() {
-  const [open,setOpen] = useState(false);
-  const [transition, setTransition] = useState("");
-
-  const navbarList = [
-    "TRANG CHỦ","ẤN PHẨM","THÔNG TIN","LIÊN HỆ"
-  ];
-  const handleMenuClick = () => {
-    setOpen(!open);
-    setTransition("transition-all transform-origin-center rotate-180");
-  };
-
-  console.log(open);
-
   return (
-    <div className='h-16 max-h-16 bg-wrapper200'>
-        <nav className="h-full px-10 flex items-center justify-between md:px-28">
-          <div className='flex-grow-0'>
-            <span>
-              <a className='font-Confortaa font-bold text-2xl' href="#">Lainovo</a>
-            </span>
-          </div>
-          <div className='md:hidden'>
-            <button 
-            className={transition}
-            onClick={handleMenuClick}>
-              <MenuLine/>
-            </button>
-          </div>
-          <div className="hidden md:block md:flex-grow">
-            <ul className='flex justify-center space-x-10'>
-              {navbarList.map((item,index) => (<li key={index} className={index === 0 ? 'font-Mulish font-bold' : 'font-Mulish font-semibold'}>{item}</li>))}
-            </ul>
-          </div>
-        </nav>
-    </div>
+    <header className='bg-red-400 w-full h-20'>
+      <nav className='bg-gray-400 flex justify-between px-10 h-full items-center'>
+        <a className='' href="/">Lainovo</a>
+        <ul className='invisible flex md:visible'>
+          <li>Home</li>
+          <li>Publications</li>
+          <li>About Us</li>
+          <li>Contact</li>
+        </ul>
+        <ButtonMenu/> 
+      </nav>
+    </header>
   );
 }
 
