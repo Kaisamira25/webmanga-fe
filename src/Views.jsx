45325f60724a535b43017cb1e1a76691f8595185
Kaisamira <thienthan726@gmail.com>
2024-02-16 10:43:42 +0700

    update develop branch

--- src/Views.jsx
+++ src/Views.jsx
@@ -3,17 +3,12 @@ import Header from "./components/_Header";
 import Search from "./components/_Search";
 import SideBar from "./components/_Sidebar";
 import Footer from './components/_Footer'
+import HomePage from "./apps/Home/Home";
 
 function Views() {
     return ( 
         <div className="bg-gradient-to-r from-[#DCEFE5] to-[#b7e9d3] h-screen">
-            {/* <Header/> */}
-            {/* <Footer/> */}
-            {/* <SideBar/> */}
-            <HeaderDemo/>
-            <img src="../src/assets/imgs/1.webp" alt="" />
-            <Search/>
-            <Footer/>
+            <HomePage/>
         </div>
      );
 }
---
+++ src/apps/Home/Home.jsx
@@ -0,0 +1,11 @@
+import HeaderDemo from "../../components/Header";
+
+function HomePage() {
+    return ( 
+        <div>
+            <HeaderDemo/>
+        </div>
+     );
+}
+
+export default HomePage;
