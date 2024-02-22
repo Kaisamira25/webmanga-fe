accdb87488ed5c2526b07f61397246bc12baf272
Kaisamira <thienthan726@gmail.com>
2024-02-22 23:37:10 +0700

    #6 - Feat: Finish Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,37 +1,46 @@
 import BtnCard from "./BtnCard";
-import CartIcon from '../assets/icons/CartIcon'
-import EyeIcon from '../assets/icons/EyeIcon'
+import CartIcon from "../assets/icons/CartIcon";
+import EyeIcon from "../assets/icons/EyeIcon";
 function Card({
   avatarCard,
   bookName,
   bookPrice,
   priceStyle,
   discountPercent,
-  summary
+  summary,
+  priceCurrent,
 }) {
   return (
-    <div className="rounded w-80 flex flex-wrap p-1 drop-shadow bg-white100">
-      <div className="basis-1/2">
-        <img src={avatarCard} alt="" />
-      </div>
-      <div className="information basis-1/2 pl-2 relative">
-        <a href="/" className="font-bold text-xl">
-          {bookName}
-        </a>
-        <div className="price flex gap-3 flex-wrap mt-4">
-          <p className={`${priceStyle} price text-sm`}>{bookPrice}</p>
-          <span className="text-sm">{discountPercent}</span>
-          <span className="text-sm">50.000 VND</span>
+    <div className="rounded w-[500px] min-h-[300px] drop-shadow bg-[#DCEFE5]">
+      <div className="h-2/3 flex">
+        <div className="basis-1/2 h-[200px]">
+          <div className="h-full flex justify-center">
+            <img className="h-full" src={avatarCard} alt="" />
+          </div>
         </div>
-        <div className="btn flex gap-5 absolute bottom-0">
-          <BtnCard btnIcon={<CartIcon/>}/>
-          <BtnCard btnIcon={<EyeIcon/>}/>
+        <div className="basis-1/2">
+          <div>
+            <div>
+              <a className="text-xl font-bold" href="/">
+                {bookName}
+              </a>
+            </div>
+            <div className="pl-1">
+              <div className="flex gap-5">
+                <p>{bookPrice}</p>
+                <p>{discountPercent}</p>
+              </div>
+              <p className="">{priceCurrent}</p>
+            </div>
+          </div>
+          <div className="flex mt-10 gap-10">
+            <BtnCard btnIcon={<CartIcon />} />
+            <BtnCard btnIcon={<EyeIcon/>}/>
+          </div>
         </div>
       </div>
-      <div className="mt-5 line-clamp-3  px-1 hover:line-clamp-none">
-        <p>
-          {summary}
-        </p>
+      <div className="mt-3 h-1/3 line-clamp-3 px-2 hover:line-clamp-none">
+        <p>{summary}</p>
       </div>
     </div>
   );
