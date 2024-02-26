2f0a4ab28ae59140703e90da3bec80d84fb7d435
Kaisamira <thienthan726@gmail.com>
2024-02-27 00:09:47 +0700

    Update: update develop branch

--- src/View.jsx
+++ src/View.jsx
@@ -1,18 +1,11 @@
-import React from "react";
-import LoginPage from "./pages/Login/LoginPage";
-import RegisterPage from "./pages/Register/RegisterPage";
-import OTPPage from "./pages/OTP/OTPPage";
-import ForgotPasswordPage from "./pages/ForgotPassword/ForgotPasswordPage";
+import Home from "./apps/Home/Home";
 
 function Views() {
-  return (
-    <div className="bg-gradient-to-r from-[#DCEFE5] to-[#b7e9d3] h-screen">
-      <LoginPage />
-      <RegisterPage />
-      <OTPPage />
-      <ForgotPasswordPage />
-    </div>
-  );
+    return ( 
+        <div>
+            <Home/>
+        </div>
+     );
 }
 
 export default Views;
