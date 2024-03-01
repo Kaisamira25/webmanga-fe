23a40b34a18fa4eb7debff742f94a355b528eb66
leanhkiet <leanhkiet249@gmail.com>
2024-03-01 21:12:18 +0700

    #14 add : cartpage raw

--- src/View.jsx
+++ src/View.jsx
@@ -1,9 +1,18 @@
+import CartPage from "./apps/Cart/CartPage";
 import Home from "./apps/Home/Home";
-
+import PopularView from "./apps/Home/component/PopularView";
+import Quantity from "./components/Quantity";
+import LoginPage from "./pages/Login/LoginPage"
+import RegisterPage from "./pages/Register/RegisterPage"
 function Views() {
     return ( 
         <div>
-            <Home/>
+            {/* <Home/> */}
+            <CartPage/> 
+            {/* <LoginPage/> */}
+            {/* <RegisterPage/> */}
+            {/* <PopularView/> */}
+            {/* <Quantity type={'number'}/> */}
         </div>
      );
 }
