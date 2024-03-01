8519e00a303a83837cc1d23915e3900fab2698b3
kitsune <patecao322@gmail.com>
2024-03-01 09:01:33 +0700

    #13 fix: margin bottom for page

--- src/View.jsx
+++ src/View.jsx
@@ -1,11 +1,16 @@
 import Home from "./apps/Home/Home";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import PaymentPage from "./pages/Payment/PaymentPage";
 
 function Views() {
-    return ( 
-        <div>
-            <Home/>
-        </div>
-     );
+  return (
+    <div>
+      <Header />
+      <PaymentPage />
+      <Footer />
+    </div>
+  );
 }
 
 export default Views;
