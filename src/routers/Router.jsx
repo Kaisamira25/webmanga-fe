42a884c0ab8c02516bb27efd8e234686c4d815f4
Do Phu <dothanhphu01012002@gmail.com>
2024-03-02 16:10:39 +0700

    #17 update: router

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -11,11 +11,15 @@
 import React, { lazy } from "react";
 const HomePage = lazy(() => import("../pages/Home/HomePage"));
 const PaymentPage = lazy(() => import("../pages/Payment/PaymentPage"));
+const UserPage = lazy(() => import("../pages/User/UserPage"));
 const ErrorPage = lazy(() => import("../pages/Error/ErrorPage"));
 
 const routes = [
   { path: "/", element: <HomePage /> },
+  { path: "/Home", element: <HomePage /> },
   { path: "/Cart/Payment", element: <PaymentPage /> },
+  { path: "/UserPage", element: <UserPage /> },
   { path: "*", element: <ErrorPage /> },
+  
 ];
 export default routes;
