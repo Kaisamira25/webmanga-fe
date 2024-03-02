d6f8a3eeee6282f715053b530e6b0a61244e7a8e
kitsune <patecao322@gmail.com>
2024-03-02 13:57:05 +0700

    #17 update: loading when wait to load page

--- src/View.jsx
+++ src/View.jsx
@@ -2,16 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import routes from "./routers/Router";
 import Header from "./components/header/Header";
+import { Suspense } from "react";
 
 function Views() {
   return (
     <div>
       <Header />
-      <Routes>
-        {routes.map(({ path, element }) => (
-          <Route key={path} path={path} element={element} />
-        ))}
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {routes.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+        </Routes>
+      </Suspense>
       <Footer />
     </div>
   );
