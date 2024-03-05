1de0299502d419c98b81b0c992fe18040793044b
kitsune <patecao322@gmail.com>
2024-03-05 15:09:37 +0700

    #11 update: update router for detail

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -1,15 +1,4 @@
-// import React from "react";
-// import PaymentPage from "../pages/Payment/PaymentPage";
-// import HomePage from "../pages/Home/HomePage";
-// import ErrorPage from "../pages/Error/ErrorPage";
-
-// const routes = [
-//   { path: "/", element: <HomePage /> },
-//   { path: "/Cart/Payment", element: <PaymentPage /> },
-//   { path: "*", element: <ErrorPage /> },
-// ];
 import React, { lazy } from "react";
-import DetailProduct from "../apps/Payment/components/DetailProduct/DetailProduct";
 import DetailsProductPage from "../pages/DetailsProduct/DetailsProductPage";
 const HomePage = lazy(() => import("../pages/Home/HomePage"));
 const PaymentPage = lazy(() => import("../pages/Payment/PaymentPage"));
@@ -17,8 +6,9 @@ const UserPage = lazy(() => import("../pages/User/UserPage"));
 const ErrorPage = lazy(() => import("../pages/Error/ErrorPage"));
 
 const routes = [
-  { path: "/", element: <DetailsProductPage /> },
+  { path: "/", element: <HomePage /> },
   { path: "/Home", element: <HomePage /> },
+  { path: "/Detail", element: <DetailsProductPage /> },
   { path: "/Cart/Payment", element: <PaymentPage /> },
   { path: "/UserPage", element: <UserPage /> },
   { path: "*", element: <ErrorPage /> },
