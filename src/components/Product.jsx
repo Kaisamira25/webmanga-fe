072416c534abf83d031e3407910dc3178f85d8a1
leanhkiet <leanhkiet249@gmail.com>
2024-03-05 19:53:18 +0700

    #14 add:router fix cartpage 4

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -9,35 +9,31 @@ function Product({
     category,
 }) {
     return (
-        <div className="rounded min-h-[300px] drop-shadow">
-            <div className="h-2/3 flex">
-                <div className="basis-1/2 h-[200px] xl:basis-1/3">
-                    <div className="h-full flex justify-center xl:pl-2 xl:pt-2 xl:justify-normal">
-                        <img className="h-full" src={Imgbook} alt="" />
+        <div className="border border-gray-200 p-4 rounded-md shadow-md flex">
+            <div className="mr-4">
+                <img className="w-24 h-auto" src={Imgbook} alt={bookName} />
+            </div>
+            <div className="flex-grow">
+                <div>
+                    <a className="text-xl font-bold">{bookName}</a>
+                </div>
+                <div className="pl-1 flex justify-between">
+                    <div>
+                        <p>{author}</p>
+                    </div>
+                    <div>
+                        <p>{bookPrice}</p>
                     </div>
                 </div>
-                <div className="basis-1/2 xl:basis-2/3">
-                    <div>
-                        <div>
-                            <a className="text-xl font-bold" href="/">
-                                {bookName}
-                            </a>
-                        </div>
-                        <div className="pl-1">
-                            <div className="flex justify-between">
-                                <p>{author}</p>
-                                <p>{bookPrice}</p>
-
-                            </div>
-                            <p className="">{category}</p>
-                        </div>
-                        <div>
-                            <Quantity/>
-                        </div>
-                    </div>
+                <div>
+                    <p className="">{category}</p>
+                </div>
+                <div>
+                    <Quantity />
                 </div>
             </div>
         </div>
     );
 }
+
 export default Product;
