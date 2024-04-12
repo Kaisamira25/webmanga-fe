4c0f4c42ded58ccc401be97d90956229cc197646
Kitsune322 <patecao322@gmail.com>
2024-04-12 10:36:05 +0700

    #17 update: guard admin page

--- src/apps/Cart/components/TotalPayment/TotalPayment.jsx
+++ src/apps/Cart/components/TotalPayment/TotalPayment.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import TextInfo from "../../../../components/TextInfo/TextInfo";
 import ButtonInput from "../../../../components/BtnInput";
 import style from "./TotalPayment.module.scss";
