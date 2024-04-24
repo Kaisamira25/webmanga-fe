e0c0b1498a215383e1777b3f59b93f0cb6afa480
Do Phu <dothanhphu01012002@gmail.com>
2024-04-24 22:10:32 +0700

    Update: Update create address

---
+++ src/assets/icons/MailIcon.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export function AkarIconsEnvelope(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24" {...props}><g fill="none" stroke="#ffffff" strokeLinejoin="round" strokeWidth="2"><path strokeLinecap="round" d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2z"></path><path d="m2 8l7.501 6.001a4 4 0 0 0 4.998 0L22 8"></path></g></svg>
+  )
+}
+export default AkarIconsEnvelope
